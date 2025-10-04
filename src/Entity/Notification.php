7495e3838a4bb29c29d2704942d175b438f2802b
TheoMoriceEM <theo.morice5@gmail.com>
2025-10-04 15:34:29 +0200

    Create notification entity

---
+++ src/Entity/Notification.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\NotificationStatus;
+use App\Repository\NotificationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NotificationRepository::class)]
+class Notification
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $recipientEmail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $subject = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $body = null;
+
+    #[ORM\Column(enumType: NotificationStatus::class)]
+    private ?NotificationStatus $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getRecipientEmail(): ?string
+    {
+        return $this->recipientEmail;
+    }
+
+    public function setRecipientEmail(string $recipientEmail): static
+    {
+        $this->recipientEmail = $recipientEmail;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): static
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getStatus(): ?NotificationStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(NotificationStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
